02ae6102ba12a6c45ab7a4fc9b233e162dd13eba
Arif Dogru <adogru@gt.edu.tr>
2019-10-14 14:05:13 +0300

    port config

--- src/core/config/index.js
+++ src/core/config/index.js
@@ -3,13 +3,14 @@ let backend;
 let frontend;
 
 if (process.env.NODE_ENV === "development") {
-  url = "mongodb://heroku_97q29l3k:123456db@ds135128.mlab.com:35128/heroku_97q29l3k";
+
+  url = "mongodb://localhost:27017/todolist";
   backend = "http://localhost:22571/v1";
   frontend = "http://localhost:3000";
 } else if (process.env.NODE_ENV === "production") {
   url = "mongodb://heroku_97q29l3k:123456db@ds135128.mlab.com:35128/heroku_97q29l3k";
-  backend = "http://localhost:22571/v1";
-  frontend = "http://localhost:3000";
+  back_end = "https://todolist-backend-arifdogru.herokuapp.com:31571/v1";
+  front_end = "https://todolist-frontend-arifdogru.herokuapp.com:33571";
 }
 
 export default {
